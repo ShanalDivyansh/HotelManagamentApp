e10c1a5e871dc994ee1cf7e099edd77e34ed63c6
ShanalDivyansh <shanaldivyansh@gmail.com>
2023-08-29 07:56:26 +0530

    Check-in Form

---
+++ HotelManagementDesktop/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using DataAccessLibrary.Data;
+using DataAccessLibrary.Databases;
+namespace HotelManagementDesktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static ServiceProvider serviceProvider;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+
+            var services = new ServiceCollection();
+            services.AddTransient<MainWindow>();
+            services.AddTransient<CheckInForm>();
+            services.AddTransient<ISQLDataAccess, SQLDataAccess>();
+            services.AddTransient<IDatabaseData, SQLData>();
+
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+
+            IConfiguration config = builder.Build();
+
+            services.AddSingleton(config);
+
+            serviceProvider = services.BuildServiceProvider();
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+            
+        }
+    }
+}
